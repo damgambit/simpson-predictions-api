4c6bf504694218083a3bcd2c713bf7fb44f25652
damgambit <damianogambitta@gmail.com>
2017-11-12 01:29:08 +0100

    cm

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
 import os
 from flask import Flask
+import keras
+
+from resnet import ResNet50
+
+model = ResNet50(input_shape = (64, 64, 3), classes = 47)
+print(model.summary())
+model.compile(optimizer=Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.002), 
+              loss='categorical_crossentropy', metrics=['accuracy'])
+
+print(model.summary())
+
+
 
 
 
@@ -8,7 +20,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/api/precict')
+@app.route('/api/predict')
 def predict():
     return jsonify({'data': 'Hello'})
 

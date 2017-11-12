7ecad15d44a5e1d410cb3655075eacf362bcdb72
damgambit <damianogambitta@gmail.com>
2017-11-12 02:24:46 +0100

    cm

--- app.py
+++ app.py
@@ -1,43 +1,29 @@
 #!/usr/bin/env python
 import os
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import keras
 from keras.optimizers import Adam
 import numpy as np
 from keras.preprocessing import image
+import json
+import keras.backend as K
+from utils import decode_img, classes
+from resnet import ResNet50
+import tensorflow as tf
 
-from resnet import ResNet50, classes
 
+# Model
+K.clear_session()
+K.set_image_data_format('channels_last')
+K.set_learning_phase(1)
 model = ResNet50(input_shape = (64, 64, 3), classes = 47)
 print(model.summary())
 model.compile(optimizer=Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.002), 
               loss='categorical_crossentropy', metrics=['accuracy'])
-
 print(model.summary())
-
-model.load_weights('model.h5')
-
-
-def decode_img(img_base64):
-  decode_str = img_base64.decode("base64")
-  file_like = cStringIO.StringIO(decode_str)
-  img = PIL.Image.open(file_like)
-  # rgb_img[c, r] is the pixel values.
-  rgb_img = img.convert("RGB")
-  return rgb_img
-
-
-def make_prediction(file):
-    test_image = image.load_img(file, target_size = (64, 64))
-    test_image = image.img_to_array(test_image)
-    test_image = np.expand_dims(test_image, axis = 0)
-    result = model.predict(test_image)
-    classes = training_set.class_indices
-    predicted_class = list(classes.keys())[list(classes.values()).index(result.argmax())]
-    
-    print("The predicted class is:", predicted_class)
-    display(Image(filename=file, width=256, height=256))
-
+model.load_weights('model.hdf5')
+graph = tf.get_default_graph()
+   
 
 
 # initialization
@@ -46,9 +32,22 @@ app = Flask(__name__)
 
 @app.route('/api/predict', methods=['POST'])
 def predict():
-	print('in')
-	print(request.data)
-	return jsonify({'prediction': 'Hello'})
+	data = request.data
+	dataDict = json.loads(data)
+	img_base64 = dataDict['image']
+
+	
+	img = np.expand_dims(decode_img(img_base64), axis=0)
+	img = np.resize(img, (1,64,64,3))
+
+	global graph
+	with graph.as_default():
+		result = model.predict(img)
+		predicted_class = list(classes.keys())[list(classes.values()).index(result.argmax())]
+    
+	print("The predicted class is:", predicted_class)
+
+	return jsonify({'prediction': predicted_class})
 
 
 if __name__ == '__main__': 
---
+++ utils.py
@@ -0,0 +1,27 @@
+import cStringIO
+import PIL.Image
+
+classes = {'abraham_grampa_simpson': 0, 'agnes_skinner': 1, 'apu_nahasapeemapetilon': 2, 
+    'barney_gumble': 3, 'bart_simpson': 4, 'bumblebee_man': 5, 'carl_carlson': 6, 
+    'charles_montgomery_burns': 7, 'chief_wiggum': 8, 'cletus_spuckler': 9, 
+    'comic_book_guy': 10, 'disco_stu': 11, 'edna_krabappel': 12, 'fat_tony': 13, 
+    'gil': 14, 'groundskeeper_willie': 15, 'hans_moleman': 16, 'helen_lovejoy': 17, 
+    'homer_simpson': 18, 'jasper_beardly': 19, 'jimbo_jones': 20, 'kent_brockman': 21, 
+    'krusty_the_clown': 22, 'lenny_leonard': 23, 'lionel_hutz': 24, 'lisa_simpson': 25, 
+    'maggie_simpson': 26, 'marge_simpson': 27, 'martin_prince': 28, 'mayor_quimby': 29, 
+    'milhouse_van_houten': 30, 'miss_hoover': 31, 'moe_szyslak': 32, 'ned_flanders': 33, 
+    'nelson_muntz': 34, 'otto_mann': 35, 'patty_bouvier': 36, 'principal_skinner': 37, 
+    'professor_john_frink': 38, 'rainier_wolfcastle': 39, 'ralph_wiggum': 40, 'selma_bouvier': 41, 
+    'sideshow_bob': 42, 'sideshow_mel': 43, 'snake_jailbird': 44, 'troy_mcclure': 45, 'waylon_smithers': 46}
+
+
+
+def decode_img(img_base64):
+  decode_str = img_base64.decode("base64")
+  file_like = cStringIO.StringIO(decode_str)
+  img = PIL.Image.open(file_like)
+  # rgb_img[c, r] is the pixel values.
+  rgb_img = img.convert("RGB")
+  return rgb_img
+
+

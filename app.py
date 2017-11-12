ca0f7c42ddd3774f7d7009b00745cd646474943f
damgambit <damianogambitta@gmail.com>
2017-11-12 03:28:07 +0100

    cm

--- app.py
+++ app.py
@@ -50,4 +50,6 @@ def predict():
 
 
 if __name__ == '__main__': 
-    app.run(host='0.0.0.0')
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
